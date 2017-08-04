01b71b8e8e3fb0a8a23d3ccc2c34eccb4d1cbe43
ayush2609 <30683960+ayush2609@users.noreply.github.com>
2017-08-04 10:53:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/articleone', function (req, res) {
-  res.send('ghtrhtrjrtj');
+  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 app.get('/article2', function (req, res) {
   res.send('g');
